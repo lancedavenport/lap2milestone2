6cd6410974bb5231f7c2ef9ad3bd6ee735a1bf60
lancedavenport <lance.davenport@icloud.com>
2023-09-24 21:28:03 -0500

    Finished

--- app/src/main/java/com/cs407/lap2milestone2/CalculatorActivity.java
+++ app/src/main/java/com/cs407/lap2milestone2/CalculatorActivity.java
@@ -2,13 +2,24 @@ package com.cs407.lap2milestone2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class CalculatorActivity extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calculator_activity);
+        textView = (TextView) findViewById(R.id.textView2);
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("message");
+        textView.setText(str);
     }
 }
--- app/src/main/java/com/cs407/lap2milestone2/MainActivity.java
+++ app/src/main/java/com/cs407/lap2milestone2/MainActivity.java
@@ -2,10 +2,60 @@ package com.cs407.lap2milestone2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public void goToActivity(String s) {
+        Intent intent = new Intent(this, CalculatorActivity.class);
+        intent.putExtra("message", s);
+        startActivity(intent);
+    }
 
+    public void addition(View view) {
+        EditText myTextField1 = (EditText) findViewById(R.id.myTextField);
+        EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
+        int a = Integer.parseInt(myTextField1.getText().toString());
+        int b = Integer.parseInt(myTextField2.getText().toString());
+        int sum = a + b;
+
+        goToActivity(Integer.toString(sum));
+
+    }
+
+    public void subtraction(View view) {
+        EditText myTextField1 = (EditText) findViewById(R.id.myTextField);
+        EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
+        int a = Integer.parseInt(myTextField1.getText().toString());
+        int b = Integer.parseInt(myTextField2.getText().toString());
+        int sum = a - b;
+        goToActivity(Integer.toString(sum));
+    }
+
+    public void multiplication(View view) {
+        EditText myTextField1 = (EditText) findViewById(R.id.myTextField);
+        EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
+        int a = Integer.parseInt(myTextField1.getText().toString());
+        int b = Integer.parseInt(myTextField2.getText().toString());
+        int sum = a * b;
+        goToActivity(Integer.toString(sum));
+    }
+    public void division(View view) {
+        EditText myTextField1 = (EditText) findViewById(R.id.myTextField);
+        EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
+        int a = Integer.parseInt(myTextField1.getText().toString());
+        int b = Integer.parseInt(myTextField2.getText().toString());
+        if (b == 0){
+            goToActivity("Cannot divide by zero");
+        } else{
+            int sum = a / b;
+            goToActivity(Integer.toString(sum));
+        }
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
